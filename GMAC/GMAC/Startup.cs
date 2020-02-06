d5c058a3f7965fa90c661cce77b67814d3a863e4
Ajay Kumar Kosuru <Ajay.Kosuru@rsystems.com>
2020-02-06 15:16:07 +0530

    code added to git

---
+++ GMAC/GMAC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GMAC.Startup))]
+namespace GMAC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
